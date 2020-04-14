d79ac559d1e7d6b2903f52ba0e912defda3ff1a9
XAlisonCruzX <alisonofdark@gmail.com>
2020-04-13 21:25:18 -0300

    Add
    imcCalc

---
+++ IMC/Aula1/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace Aula1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtAltura = new System.Windows.Forms.TextBox();
+            this.txtPeso = new System.Windows.Forms.TextBox();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblInd = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Altura";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Peso";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(65, 134);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Calcular";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtAltura
+            // 
+            this.txtAltura.Location = new System.Drawing.Point(65, 38);
+            this.txtAltura.Name = "txtAltura";
+            this.txtAltura.Size = new System.Drawing.Size(100, 20);
+            this.txtAltura.TabIndex = 3;
+            // 
+            // txtPeso
+            // 
+            this.txtPeso.Location = new System.Drawing.Point(65, 85);
+            this.txtPeso.Name = "txtPeso";
+            this.txtPeso.Size = new System.Drawing.Size(100, 20);
+            this.txtPeso.TabIndex = 4;
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(108, 186);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(13, 13);
+            this.lblResultado.TabIndex = 5;
+            this.lblResultado.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(44, 186);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Resultado:";
+            // 
+            // lblInd
+            // 
+            this.lblInd.AutoSize = true;
+            this.lblInd.Location = new System.Drawing.Point(62, 258);
+            this.lblInd.Name = "lblInd";
+            this.lblInd.Size = new System.Drawing.Size(14, 13);
+            this.lblInd.TabIndex = 7;
+            this.lblInd.Text = "\'-\'";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(207, 291);
+            this.Controls.Add(this.lblInd);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.txtPeso);
+            this.Controls.Add(this.txtAltura);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "s";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtAltura;
+        private System.Windows.Forms.TextBox txtPeso;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblInd;
+    }
+}
+
---
+++ IMC/Aula1/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aula1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public string getInd(float imc)
+        {
+            string mensagem = "";
+            
+            if (imc <= 15)
+            {
+                mensagem = "Extremamente abaixo do peso";
+            }
+            else if (imc <= 16)
+            {
+                mensagem = "Gravemente abaixo do peso";
+            }
+            else if (imc <= 18.5)
+            {
+                mensagem = "Abaixo do peso ideal";
+            }
+            else if (imc <= 25)
+            {
+                mensagem = "Faixa de peso ideal";
+            }
+            else if (imc <= 30)
+            {
+                mensagem = "Sobrepeso";
+            }
+            else if (imc <= 35)
+            {
+                mensagem = "Obesidade grau 1";
+            }
+            else if (imc <= 40)
+            {
+                mensagem = "Obesidade grau 2";
+            }
+            else if (imc > 40)
+            {
+                mensagem = "Obesidade grau 3";
+            }
+            return mensagem;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                float altura = float.Parse(txtAltura.Text);
+                float peso = float.Parse(txtPeso.Text);
+                float imc = peso / (altura * altura);
+                lblInd.Text = getInd(imc);
+                lblResultado.Text = Convert.ToString(Math.Round(imc, 2));
+            }
+            catch (FormatException)
+            {
+                lblResultado.Text = "Agument NULL";
+            }
+           
+
+        }
+
+        
+    }
+}
